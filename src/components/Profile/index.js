c5a0c7e1e08aaad7b58fab54bf3fb06a4af93454
Daniel Alejandro Rodriguez Gomez <daniel@DANIEL.WORKSPACE>
2020-08-27 18:24:51 -0500

    Nuevo cambio visual pagina web

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -21,15 +21,22 @@ const ProfileComponent = () => {
                 <Divider />
                 <Container fluid>
                     <Header as='h3'>Breve descripción</Header>
+                    <Divider />
                     <p align="justify">
                         Soy un gran apasionado del desarrollo de software. Cuento con 2 años y medio laborando en desarrollo fullstack de aplicaciones web, he contado con enriquecedoras experiencias laborales; en 2017 tuve la oportunidad de participar en un proyecto ganador del premio Duke's Choice Award que celebra Oracle para las comunidades de programadores Java. En el ultimo proyecto que participé pudimos desplegar una aplicación legacy elaborada en Spring y React a la nube con AWS, se refactorizó algunas partes del codigo con mejores practicas y mejoramos notablemente la deuda tecnica, abarcamos el proyecto con Hacking Continuo, dimos soporte a bugs y desarrollamos nuevos requerimientos levantados por el negocio.
                     </p>
+                    <Header as='h3'>Experiencia Desarrollo Backend</Header>
+                    <Divider />
                     <p align="justify">
                         Tengo experiencia trabajando por parte del Backend en desarrollo de aplicaciones web con las especificaciones de Java EE (JPA, EJB, Servlets, etc.) con arquitectura monoliticas usando JSF para la capa del cliente y tambien con web services con JAX-WS y JAX-RS usando Jersey. Tambien he trabajado con Spring Framework en web services utilizando en la capa de datos el ORM Hibernate, tambien con el desarrollo de pruebas unitarias con JUnit, Mockito y Powermockito y pruebas de aceptación con SOAP UI ademas de testear el API con Postman y documentarlo con Swagger. Tengo conocimientos con Spring MVC y Thymeleaf para aplicaciones monoliticas y en arquitecturas de microservicios con SpringBoot y con las tecnologias cloud de Netflix como Eureka, Hystrix, Zuul, entre otras.
                     </p>
+                    <Header as='h3'>Experiencia Desarrollo Frontend</Header>
+                    <Divider/>
                     <p align="justify">
                         En Frontend cuento con 1 año y medio de experiencia trabajando con Javascript en React.js y manejo de estados con Redux, manejo de los formularios con Redux Form y Formik, tambien con el desarrollo de pruebas unitarias con Mocha, Chai, Enzyme y Sinon. Tengo conocimientos en React Hooks y la nueva implementación de React Redux con Hooks de versiones mayores a 16.8.
                     </p>
+                    <Header as='h3'>Experiencia en metodologías de trabajo, Cloud y DevOps</Header>
+                    <Divider/>
                     <p align="justify">
                         En metodología he trabajado bajo el marco de trabajo Scrum con el rol de Solucionador y en DevOps ejecutando los CI y CD para su correspondiente integracion y despliegue en ambientes intermedios y produccion en un proyecto con 100% de adopción con la herramienta de Azure Visual Studio Team Services (VSTS). En temas de nube he trabajado con AWS para analisis de logs y de disponibilidad del servicio con CloudWatch, tambien tengo conocimiento en despliegues de paginas web estáticas y alojamiento de información en S3, creación y manipulación de Instancias EC2 para despliegues de aplicaciones y servicios web.
                     </p>
--- src/template/index.js
+++ src/template/index.js
@@ -1,36 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './style.css';
 import ContactComponent from '../components/Contact';
 import ProfileComponent from '../components/Profile';
 import CatalogComponent from '../components/Catalog';
-import { Icon } from "semantic-ui-react";
+import { Icon, Popup } from "semantic-ui-react";
 import { version } from './../../package.json';
 
 const Template = () => {
+    const [toggle, setToggle] = useState(true);
+    const scrollEvent = () => {
+        let header = document.querySelector('.lateralMenu');
+        header.classList.toggle("sticky", window.scrollY > header.style.height);
+    }
+    useEffect(()=> {
+        window.addEventListener("scroll", () => scrollEvent());
+    },[]);
+    const hamburguerToggle = () => {
+        let menu = document.querySelector('.lateralMenu');
+        setToggle(!toggle);
+        toggle ? 
+            menu.classList.replace("toggleMenuHide","toggleMenuShow")
+        : menu.classList.replace('toggleMenuShow','toggleMenuHide');
+    }
     return(
         <Router>
             <div className="mainContainer animate__animated animate__fadeIn animate__delay-2">
-                <div className="lateralMenu">
-                    <h1 style={{textAlign:"left", fontWeight:"900"}} className="title">Daniel Rodriguez's <br/>Page</h1>
-                    <ul>
-                        <li id="itemMenu"><h3><Icon size="large" name="hand peace"/><Link to="/"> Acerca de mí</Link></h3></li>
-                        <li id="itemMenu"><h3><Icon size="large" name="heart"/><Link to="/catalog"> Habilidades</Link></h3></li>
-                        <li id="itemMenu"><h3><Icon size="large" name="address book"/><Link to="/contact"> Contacto</Link></h3></li>
-                    </ul>
-                    <p className="extraInfoPage">
-                        Esta página es elaborada<br/> únicamente con React.js <br/>
-                        por Daniel Rodriguez. <br/>
-                        <Icon size="big" name="github"/><a href="https://github.com/danielrg97/personal-web-page">Ver codigo fuente</a>
-                        <br/>
-                        <span style={{color: "lightgrey", fontSize: "8"}}>Version {version}</span>
-                    </p>
-                </div>
+                <div class="allHeader">
+                <Popup
+                    trigger={<button id="hamburguer" onClick={() => hamburguerToggle()}/> }
+                    content='Menú principal'
+                    inverted
+                    style={{opacity: 0.7}}
+                    position='bottom left'
+                />
+                    <header className="lateralMenu toggleMenuHide">
+                        <h1 style={{textAlign:"left", fontWeight:"900"}} className="title">Daniel <br/>Rodriguez's <br/>Page</h1>
+                        <ul>
+                            <li id="itemMenu"><h3><Icon size="large" name="hand peace"/><br/><Link to="/"> Acerca de mí</Link></h3></li>
+                            <li id="itemMenu"><h3><Icon size="large" name="heart"/><br/><Link to="/catalog"> Habilidades</Link></h3></li>
+                            <li id="itemMenu"><h3><Icon size="large" name="address book"/><br/><Link to="/contact"> Contacto</Link></h3></li>
+                        </ul>
+                        <Link class="onlyAdmin">Admin Dashboard</Link>{/** TODO: usuario en sesion debe verse aqui*/}   
+                    {/*
+                        
+                        */}
+                    </header>
+                </div> 
                 <div className="fragment">
                     <Route exact path="/" component={ProfileComponent} />
                     <Route path="/contact" component={ContactComponent} />  
                     <Route path="/catalog" component={CatalogComponent} />  
                     <h2 style={{textAlign:"center", padding: "1.5em", color:"lightgrey"}}><Icon name="angle down"/>Aquí termina esta sección<Icon name="angle down"/></h2>
+                    <div className="extraInfoPage">
+                            Esta página es elaborada<br/> con React.js y Javascript Vanilla <br/>
+                            <Icon size="big" name="github"/><a href="https://github.com/danielrg97/personal-web-page">Ver codigo fuente</a>
+                            <br/>
+                            <span style={{color: "lightgrey", fontSize: "8"}}>Version {version}</span>
+                    </div>
                 </div>
             </div>
         </Router>
